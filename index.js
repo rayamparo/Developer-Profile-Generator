238d46d942ee2fe67fb76a134a67c65ca13268ef
Raymond Amparo <ray.amparo1995@gmail.com>
2020-02-15 22:18:30 -0800

    Second Commit

--- index.js
+++ index.js
@@ -1,156 +1,53 @@
-// const fs = require("fs");
-// const inquirer = require("inquirer");
-// const axios = require("axios");
+const fs = require("fs");
+const inquirer = require("inquirer");
+const axios = require("axios");
 
-// const questions = [
-//     {
-//         name: "username",
-//         message: "What is your GitHub username?",
-//         type: "input"
-//     },
-//     {
-//         name: "project",
-//         message: "What is your project name?",
-//         type: "input"
-//     },
-//     {
-//         name: "description",
-//         message: "Please write a short description of your project",
-//         type: "input"
-//     },
-//     {
-//         name: "licenses",
-//         message: "What kind of license should your project have?",
-//         type: "list",
-//         choices: ["MIT", "APACHE 2.0", "GPL 3.0", "BSD 3", "None"]
-//     },
-//     {
-//         name: "install",
-//         message: "What command should be run to install dependencies?",
-//         type: "input"
-//     },
-//     {
-//         name: "tests",
-//         message: "What command should be run to run tests?",
-//         type: "input"
-//     },
-//     {
-//         name: "repo",
-//         message: "What does the user need to know about using the repo?",
-//         type: "input"
-//     },
-//     {
-//         name: "contribute",
-//         message: "What does the user need to know about contributing to the repo?",
-//         type: "input"
-//     }
-// ];
-
-// function writeToFile(fileName, data){
-//     console.log(`${fileName}.md`);
-//     console.log(data);
-// }
-
-const axios = require("axios")
-const inquire = require('inquirer')
-const fs = require('fs');
-//where i put all the inquirer enabled questions, properties were message name type and choices 
 const questions = [
-{
-    message: 'what is your Github username?',
-    name: 'username',
-    type: 'input'
-},
-{
-    message: 'what is your project title?',
-    name: 'title',
-    type: 'input'
-},
-{
-    message: "what's the purpose of your project? why does it even exist?",
-    name: 'purpose',
-    type: 'input'
-},
-{
-    message: 'what sections would you like to include in your project?',
-    name: 'table',
-    type: 'checkbox',
-    choices: ['Intro','Body','Appendix','Conclusion','Epilogue', 'Bibliography']
-},
-{
-    message: 'How would someone install your application?',
-    name: 'installation',
-    type: 'list',
-    choices: ['npm','yarn','clone and go','BitTorrent']
-},
-{
-    message: 'What license are you using?',
-    name: 'license',
-    type: 'list',
-    choices: ['MIT License','GNU','To Kill','Eclipse']
-},
-{
-    message: 'Who are the contributors to this project?',
-    name: 'contributing',
-    type: 'input'
-}
-
-];
-//look up day 1 formatting of results...
-function writeToFile(fileName, data) {
-console.log(`${fileName}.md`);
-console.log(data);
-let answerTable = ""
-//for loop to seperate out the outline answers into seperate bullet points...
-for (let i=0; i < data.answers.table.length; i++) {
-    answerTable += `* ${data.answers.table[i]}` + "\n"
-}
-let readmeContent = 
-`
- # **${data.answers.title}**
- ## Project Purpose 
- * ### ${data.answers.purpose}
- ## Project Outline 
- ${answerTable}
- ## Project Installation
- * ### ${data.answers.installation} is necessary for the installation.
- ## Git User Info
- > ### ${data.answers.username} \n
- * <img src="${data.avatar_url}"><img>
- ## Project Licensing
- > __${data.answers.license}__
- ## Project Contributors
- > Thanks to ${data.answers.contributing} for contributing to this project. 
- > Without you, none of **this** would have been possible.   
- `
-fs.writeFile(`${fileName}.md`, readmeContent, function(err){
-    if(err){
-        console.log(err)
-        throw err
-    }else{
-        console.log('success')
+    {
+        name: "username",
+        message: "What is your GitHub username?",
+        type: "input"
+    },
+    {
+        name: "project",
+        message: "What is your project name?",
+        type: "input"
+    },
+    {
+        name: "description",
+        message: "Please write a short description of your project",
+        type: "input"
+    },
+    {
+        name: "licenses",
+        message: "What kind of license should your project have?",
+        type: "list",
+        choices: ["MIT", "APACHE 2.0", "GPL 3.0", "BSD 3", "None"]
+    },
+    {
+        name: "install",
+        message: "What command should be run to install dependencies?",
+        type: "input"
+    },
+    {
+        name: "tests",
+        message: "What command should be run to run tests?",
+        type: "input"
+    },
+    {
+        name: "repo",
+        message: "What does the user need to know about using the repo?",
+        type: "input"
+    },
+    {
+        name: "contribute",
+        message: "What does the user need to know about contributing to the repo?",
+        type: "input"
     }
-} )
+];
+
+function writeToFile(fileName, data){
+    console.log(`${fileName}.md`);
+    console.log(data);
 }
 
-function init() {
-    inquire.prompt(questions)
-    .then(answers=>{
-        const queryUrl = `https://api.github.com/users/${answers.username}`;
-
-        axios.get(queryUrl).then(function(res) {
-          console.log(res.data);
-          console.log(res.data.login);
-          writeToFile(answers.username, {...res.data, answers})
-          })
-          .catch(error => {
-            console.log(error)
-          })
-    })
-    .catch(error => {
-        console.log(error)
-      })
-    
-}
-
-init();
